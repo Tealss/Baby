adfffe74bebc9aab56d35b190452cda7f312c9d3
Tealss <aitan@naver.com>
2023-09-25 11:00:54 +0900

    이동 수정
    수정

---
+++ Assets/Scripts/Manager/Gamemanager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamemanager : MonoBehaviour
+{
+    public static Gamemanager instance;
+    public player player;
+    
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
