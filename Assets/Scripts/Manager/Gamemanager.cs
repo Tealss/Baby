9055b222da2031dd674014044c3be894ed268f0a
LeeHaTaeg <gkxor6457@gmail.com>
2023-09-26 17:10:17 +0900

    딱히 한거 없음

--- Assets/Scripts/Manager/Gamemanager.cs
+++ Assets/Scripts/Manager/Gamemanager.cs
@@ -8,13 +8,19 @@ public class Gamemanager : MonoBehaviour
 {
     public static Gamemanager instance;
     public player player;
-    
-    public List<Gameobject> rewards = new List<Gameobject>();
+
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPostions = new List<Transform>();
+    public List<GameObject> rewards = new List<GameObject>();
 
     void Awake()
     {
         instance = this;
-       
+
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPostions.Add(spawnPositionsRoot.GetChild(i));
+        }
     }
 
     void Start()
@@ -28,6 +34,10 @@ public class Gamemanager : MonoBehaviour
     }
     void CreateReward()
     {
+        int idx = UnityEngine.Random.Range(0, rewards.Count);
+        int posIdx = UnityEngine.Random.Range(0, spawnPostions.Count);
 
+        GameObject obj = rewards[idx];
+        Instantiate(obj, spawnPostions[posIdx].position, Quaternion.identity);
     }
 }
