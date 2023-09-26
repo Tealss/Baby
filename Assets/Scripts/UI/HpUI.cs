92ef13056e7e62496c4a375aa4921c7c9cbfd931
LeeHaTaeg <gkxor6457@gmail.com>
2023-09-26 10:04:34 +0900

    UI 리소스 추가

---
+++ Assets/Scripts/UI/HpUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpUI : MonoBehaviour
+{
+    private GameObject[] heartContainers;
+    private Image[] heartFills;
+
+
+}
