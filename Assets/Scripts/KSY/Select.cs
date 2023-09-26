005d4d0da6f1908fa0c0f7a74e511b077367aaf9
Tealss <aitan@naver.com>
2023-09-26 22:05:17 +0900

    ㄴㄴㄴ
    ㄴㄴㄴ

--- Assets/Scripts/KSY/Select.cs
+++ Assets/Scripts/KSY/Select.cs
@@ -7,72 +7,64 @@ public class Select : MonoBehaviour
 {
     [Header("# Object ")]
     public GameObject[] characterPrefabs; //  Prefabs 배열
-    public Transform[] spawnPoints; // 캐릭터 생성 위치
-    public Transform[] EndPoints; // 캐릭터 생성 위치
-    public Transform BossPoint; // 캐릭터 생성 위치
-    // public List<Vector3> Teleport;
     private GameObject currentCharacter;   // 현재 캐릭
-    private Vector3 previousPosition;
-    public int Max;
-    public int Rand;
+    
+    public List<Vector3> Enter;
+    public List<Vector3> Exit;
+    public Vector3 Boss;
+    public int Draw;
 
     private void Start()
     {
-        Max = spawnPoints.Length;
-        //Teleport.Add(new Vector3(-36.3f, 8.7f, 0));
-        //Teleport.Add(new Vector3(-66.4f, -1.5f, 0));
-        //Teleport.Add(new Vector3(-97.4f, -2.8f, 0));
-        // 캐릭터 생성
+
+        // 소환 좌표 추가
+        Enter.Add(new Vector3(-36.3f, 8.7f, 0));
+        Enter.Add(new Vector3(-66.4f, -1.5f, 0));
+        Enter.Add(new Vector3(-97.4f, -2.8f, 0));
+        
+        // 출구 좌표 추가
+        Exit.Add(new Vector3(-43.11f, 14.61f, 0));
+        Exit.Add(new Vector3(-65.39f, 14.69f, 0));
+        Exit.Add(new Vector3(-95.73f, 4.26f, 0));
+
+        Boss = new Vector3(-127.8f, -3.2f, 0);
+
         SpawnCharacter(0);
     }
-
+   
     private void SpawnCharacter(int characterIndex)
     {
 
-        // 새로운 캐릭터 생성을 위해 캐릭 삭제
-        if (currentCharacter != null)
+        Destroy(currentCharacter);
+
+        // Enter.Count 는 소환 좌표의 총 갯수를 의미함
+        // 총 갯수가 0 이 아닐때, 즉 모든 스테이지를 클리어하지 않을때에는 true 그렇지 않으면 false
+        if (Enter.Count != 0)
         {
-            previousPosition = currentCharacter.transform.position;
-            Destroy(currentCharacter);
-        }
-
-        // 인트 랜덤 스폰지역
-        if (Max > 0)
-        {
-            int Rand = Random.Range(0, Max);
-
-            //int Dochack = Random.Range(0, Teleport.Count);
-            //currentCharacter = Instantiate(characterPrefabs[characterIndex], Teleport[Dochack], Quaternion.identity);
-            //Teleport.RemoveAt(Dochack);
-
-            // 현재 캐릭값의 위치를 잡아서, 그 값(previousPosition)으로 생성함 // 변경 스폰포인트
-            currentCharacter = Instantiate(characterPrefabs[characterIndex], spawnPoints[Rand].position, Quaternion.identity);
-            //    Debug.Log(spawnPoints[Rand]);
-            //    Debug.Log(EndPoints[Rand]);
+            // Draw 를 0 ~ 남은 스테이지 수 랜덤 수 출력함 + 캐릭터 삭제 후 생성
+            Draw = Random.Range(0, Enter.Count);
+            currentCharacter = Instantiate(characterPrefabs[characterIndex], Enter[Draw], Quaternion.identity);
+            Enter.RemoveAt(Draw);
         }
         else
         {
-            currentCharacter = Instantiate(characterPrefabs[characterIndex], BossPoint.position, Quaternion.identity);
+            currentCharacter = Instantiate(characterPrefabs[characterIndex], Boss, Quaternion.identity);
         }
     }
-    //그거 좌표 다른이유 암 그 좌표가 폴더가 밑에 내려오는건 기준 좌표가 위에 좌표에서 좌표가 바뀐거임
 
     public void Update()
     {
-        Transform My = currentCharacter.transform;
-        float Distance = Vector3.Distance(My.position, EndPoints[Rand].position);
-        Debug.Log(Distance);
-        if (Distance <= 1)
+        if (Exit.Count != 0)
         {
-            Max = Max - 1;
-            spawnPoints[Rand] = spawnPoints[Max];
-            EndPoints[Rand] = EndPoints[Max];
-            SpawnCharacter(0);
+            if (Vector3.Distance(currentCharacter.transform.position, Exit[Draw]) <= 4)
+            {
+                Exit.RemoveAt(Draw);
+                SpawnCharacter(0);
+            }
         }
     }
     public void ChangeCharacter(int characterIndex)
     {
-
         // 캐릭 변경
         SpawnCharacter(characterIndex);
     }
