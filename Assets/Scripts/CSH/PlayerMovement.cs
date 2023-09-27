bac4a46999f7d2e98c620a2267747528e2a703d6
SeoJaeMin619 <emiya1663@naver.com>
2023-09-27 16:53:01 +0900

    Revert "제발2"
    This reverts commit be73355449cfb4a7f7aa64b918dcc685cfc42e00.

--- Assets/Scripts/CSH/PlayerMovement.cs
+++ Assets/Scripts/CSH/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour
 {
     protected CharacterController Controller;
     protected Rigidbody2D Rigidbody2D;
-<<<<<<< HEAD
     protected SpriteRenderer SpriteRenderer;
     protected Animator Animator;
     protected Vector2 MovementDirection = Vector2.zero;
@@ -19,25 +18,6 @@ public class PlayerMovement : MonoBehaviour
         Rigidbody2D = GetComponent<Rigidbody2D>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
         Animator = GetComponentInChildren<Animator>();
-=======
-
-    protected Vector2 MovementDirection = Vector2.zero;
-
-
-    [Header("Body")]
-    public Transform Body;
-    Animator BodyAnimator;
-    SpriteRenderer BodySpriteRenderer;
-
-
-
-    protected virtual void Start()
-    {  
-        Controller = GetComponent<CharacterController>();
-        Rigidbody2D = GetComponent<Rigidbody2D>();
-        BodySpriteRenderer = Body.GetComponent<SpriteRenderer>();
-        BodyAnimator = Body.GetComponent<Animator>();
->>>>>>> SJM_New2
         Controller.OnMoveEvent += Move;
     }
     protected virtual void FixedUpdate()
@@ -50,44 +30,20 @@ public class PlayerMovement : MonoBehaviour
         MovementDirection = direction;
         if(direction.x < 0) 
         {
-<<<<<<< HEAD
             transform.rotation = Quaternion.Euler(0, 0, 90);
         }
         else if(direction.x > 0) 
         {
             transform.rotation = Quaternion.Euler(0, 0, -90);
-=======
-            
-            BodySpriteRenderer.flipX = true;
-            BodyAnimator.SetBool("isRun_R", true);
-            
-        }
-        else if(direction.x > 0) 
-        {
-            BodyAnimator.SetBool("isRun_R", true);
-            BodySpriteRenderer.flipX = false;
->>>>>>> SJM_New2
         }
         if (direction.y < 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 180);
         }
         else if (direction.y > 0)
-<<<<<<< HEAD
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
-=======
-        {       
-            BodyAnimator.SetBool("isRun_UP", true);           
-        }
-        else
-        {
-            BodyAnimator.SetBool("isRun_UP", false);
-            BodyAnimator.SetBool("isRun_R", false);
-        }
-        
->>>>>>> SJM_New2
 
     }
 
--- Assets/Scripts/SJM/Monster.cs
+++ Assets/Scripts/SJM/Monster.cs
@@ -17,14 +17,7 @@ public class Monster : MonoBehaviour
 
     public Slider mySlider;
 
-<<<<<<< HEAD
     private int _MaxHp;
-=======
-    public static float HP;
-    public static float speed;
-    public static float AD;
-    Rigidbody2D rigid;
->>>>>>> SJM_New2
 
     private int _Hp;
     private float _Speed;
@@ -33,7 +26,6 @@ public class Monster : MonoBehaviour
 
     public void Init(int hp, float speed)
     {
-<<<<<<< HEAD
         _rigidbody = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
         //_target = Gameobject;  //플레이어 추격
@@ -42,17 +34,10 @@ public class Monster : MonoBehaviour
         _Speed = speed;
         mySlider.value = _Hp / _MaxHp;
         _rigidbody.velocity = Vector2.zero;
-=======
-        HP = 100;
-        rigid = GetComponent<Rigidbody2D>();
-        speed = 3;
-        AD = 10.0f;
->>>>>>> SJM_New2
     }
 
     void FixedUpdate()
     {
-<<<<<<< HEAD
         if (_IsHit)
         {
             _rigidbody.velocity = (_target.position - _rigidbody.position) * -2;
@@ -102,18 +87,5 @@ public class Monster : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         _IsHit = false;
-=======
-        // 몬스터의 체력이 0 이하로 떨어지면 파괴
-        if (HP <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    // 다른 스크립트에서 호출하여 몬스터의 체력을 감소시키는 메서드
-    public void TakeDamage(float damage)
-    {
-        HP -= damage;
->>>>>>> SJM_New2
     }
 }
