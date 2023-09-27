280dfb1f188451176572f5a5f12ea2128d24ebff
SeoJaeMin619 <emiya1663@naver.com>
2023-09-27 16:53:33 +0900

    Revert "제발2"
    This reverts commit be73355449cfb4a7f7aa64b918dcc685cfc42e00.

--- Assets/Scripts/CSH/Bullet.cs
+++ Assets/Scripts/CSH/Bullet.cs
@@ -5,65 +5,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     protected float speed = 10f;
-<<<<<<< HEAD
     public Rigidbody2D Rigidbody;
     public void DestoryBulletInvoke()
     {
         Invoke(nameof(DestroyBullet), 5f);
-=======
-    Rigidbody2D Rigidbody;
-    Animator animator;
-
-    WaitForSeconds WaitSeconds = new WaitForSeconds(1f);
-    private void Awake()
-    {
-        Rigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
-    public void DestoryBulletInvoke()//낙하 대기
+    private void DestroyBullet()
     {
-        Rigidbody.gravityScale = 1.9f;
-        Invoke(nameof(DestroyBullet), 0.2f);
->>>>>>> SJM_New2
-    }
-    private void DestroyBullet()//낙하
-    {
-<<<<<<< HEAD
-=======
-        Rigidbody.gravityScale = 0f;
-        Rigidbody.velocity = Vector2.zero;
-        animator.Play("Destroy");
-        StartCoroutine(DelayDestroy());
-    }
-    IEnumerator DelayDestroy()//사라지는 애니메이션 대기
-    {
-        yield return WaitSeconds;
->>>>>>> SJM_New2
         ObjectPool.Instance.ReturnObj(this);
     }
-    public void MoveUp()
+    public void Move(Vector2 Direction)
     {
-<<<<<<< HEAD
         transform.up = Direction;
         Rigidbody.velocity = Direction * speed;
-=======
-        Rigidbody.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
->>>>>>> SJM_New2
     }
-    public void MoveDown()
-    {
-        Rigidbody.AddForce(Vector2.down * speed, ForceMode2D.Impulse);
-    }
-    public void MoveLeft()
-    {
-        Rigidbody.AddForce(Vector2.left * speed, ForceMode2D.Impulse);
-    }
-    public void MoveRight()
-    {
-        Rigidbody.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
-    }
-
-
 
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -71,7 +26,7 @@ public class Bullet : MonoBehaviour
 
         if (other.tag == "Player")
         {
-            //DestroyBullet();
+            DestroyBullet();
         }
         else if (other.tag == "Enemy" || other.tag == "Boss")
         {
--- Assets/Scripts/CSH/PlayerController.cs
+++ Assets/Scripts/CSH/PlayerController.cs
@@ -17,26 +17,9 @@ public class PlayerController : CharacterController
         CallMoveEvent(value.Get<Vector2>());
     }
 
-    public void OnShoot()
+    public void OnShoot(InputValue value)
     {
-<<<<<<< HEAD
         IsShooting = value.isPressed;
-=======
-        //if (_timeSinceLastAttack < 0.5f)//ÃÑ¾Ë ¼Óµµ
-        //{
-        //    _timeSinceLastAttack += Time.deltaTime;
-        //}
-        //else if (IsShooting && _timeSinceLastAttack >= 0.5f)
-        //{
-        //    CallShootEvent(value.Get<Vector2>());
-        //    _timeSinceLastAttack = 0;
-        //}
-        if (_timeSinceLastAttack >= 0.2f)
-        {
-            CallShootEvent();
-            _timeSinceLastAttack = 0;
-        }
->>>>>>> SJM_New2
 
     }
 }
