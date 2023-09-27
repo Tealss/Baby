e64fd2ab09abb1a5831d9cae81e1bb45ab395a0c
SeoJaeMin619 <emiya1663@naver.com>
2023-09-27 11:30:27 +0900

    ㅇㅇ

--- Assets/Scripts/SJM/Monster.cs
+++ Assets/Scripts/SJM/Monster.cs
@@ -6,25 +6,40 @@ using UnityEngine.InputSystem.XR;
 
 public class Monster : MonoBehaviour
 {
+    public float startingHP = 100; // 몬스터의 초기 체력
+    public float speed = 3;
+    public float AD = 10.0f;
 
-
-    public static float HP;
-    public static float speed;
-    public static float AD;
-    Rigidbody2D rigid;
+    private float currentHP; // 현재 체력
 
     void Start()
     {
-
-        HP = 100;
-        rigid = GetComponent<Rigidbody2D>();
-        speed = 3;
-        AD = 10.0f;
-
+        currentHP = startingHP;
     }
 
     void Update()
     {
+        // 몬스터의 체력이 0 이하로 떨어지면 파괴
+        if (currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
+    // 다른 오브젝트(총알 등)이 몬스터와 충돌할 때 호출되는 메서드
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bullet")) // 충돌한 오브젝트가 총알인 경우
+        {
+            // 총알의 데미지를 몬스터의 체력에서 빼기
+            Bullet bullet = other.GetComponent<Bullet>();
+            if (bullet != null)
+            {
+                currentHP -= bullet.damage;
+            }
+
+            // 총알 파괴
+            Destroy(other.gameObject);
+        }
     }
 }
